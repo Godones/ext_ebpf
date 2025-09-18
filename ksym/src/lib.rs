f2f0b6afff6ed3d2858552d92e07f50ef341455d
Godones <chenlinfeng25@outlook.com>
2025-09-18 20:15:24 +0800

    feat: Refactor the ksym interface
    Signed-off-by: Godones <chenlinfeng25@outlook.com>

--- ksym/src/lib.rs
+++ ksym/src/lib.rs
@@ -1,71 +1,64 @@
 #![no_std]
 #![feature(linkage)]
 
-use alloc::format;
-use alloc::string::String;
-use core::ffi::CStr;
+#[cfg(feature = "assembly")]
+mod assembly;
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
+
+#[cfg(feature = "assembly")]
+pub use assembly::init_kernel_symbols;
+use lazyinit::LazyInit;
 extern crate alloc;
 
-#[linkage = "weak"]
-#[no_mangle]
-fn ksyms_address() {}
-#[linkage = "weak"]
-#[no_mangle]
-fn ksyms_num() {}
-#[linkage = "weak"]
-#[no_mangle]
-fn ksyms_names_index() {}
-#[linkage = "weak"]
-#[no_mangle]
-fn ksyms_names() {}
+pub static KSYM: LazyInit<Vec<(String, usize)>> = LazyInit::new();
 
-/// Return the func infomation according to the pc address and
-pub unsafe fn lookup_kallsyms(addr: u64, level: i32) -> String {
-    let sym_names = ksyms_names as *const u8;
-    let sym_num = ksyms_num as usize;
-    let kallsyms_address_list = core::slice::from_raw_parts(ksyms_address as *const u64, sym_num);
-    let sym_names_index = ksyms_names_index as *const u64;
-    let sym_names_index = core::slice::from_raw_parts(sym_names_index, sym_num);
+/// Initialize the kernel symbol table from a string in the format of
+/// /proc/kallsyms
+#[cfg(not(feature = "assembly"))]
+pub fn init_kernel_symbols(ksym: &str) {
+    let mut symbol_table = Vec::new();
+    for line in ksym.lines() {
+        let mut parts = line.split_whitespace();
+        let vaddr = usize::from_str_radix(parts.next().unwrap_or("0"), 16).unwrap_or(0);
+        let symbol_type = parts.next().and_then(|s| s.chars().next()).unwrap_or(' ');
+        let symbol = parts.collect::<Vec<_>>().join(" ");
+        if symbol_type == 'T' || symbol_type == 't' {
+            symbol_table.push((symbol, vaddr));
+        }
+    }
+
+    KSYM.init_once(symbol_table);
+}
+
+/// Return the function information according to the pc address.
+/// If not found, return None.
+pub fn lookup_kallsyms(addr: usize) -> Option<(String, usize)> {
     let mut index = usize::MAX;
+    let ksym = KSYM.get().unwrap();
+    let sym_num = ksym.len();
     for i in 0..sym_num - 1 {
-        if addr > kallsyms_address_list[i] && addr <= kallsyms_address_list[i + 1] {
+        if addr > ksym[i].1 && addr <= ksym[i + 1].1 {
             index = i;
             break;
         }
     }
     if index < sym_num {
-        let sym_name = CStr::from_ptr(sym_names.add(sym_names_index[index] as usize) as _)
-            .to_str()
-            .unwrap();
-        format!(
-            "[{}] function:{}() \t(+) {:04} address:{:#018x}",
-            level,
-            sym_name,
-            addr - kallsyms_address_list[index],
-            addr
-        )
-    } else {
-        format!(
-            "[{}] function:unknown \t(+) {:04} address:{:#018x}",
-            level,
-            addr - kallsyms_address_list[sym_num - 1],
-            addr
-        )
+        let sym_name = ksym[index].0.as_str();
+        Some((sym_name.to_string(), ksym[index].1));
     }
+    None
 }
 
-/// Get the address of the symbol
-pub unsafe fn addr_from_symbol(symbol: &str) -> Option<u64> {
-    let sym_num = ksyms_num as usize;
-    let sym_names = ksyms_names as *const u8;
-    let kallsyms_address_list = core::slice::from_raw_parts(ksyms_address as *const u64, sym_num);
-    let sym_names_index = ksyms_names_index as *const u64;
-    let sym_names_index_list = core::slice::from_raw_parts(sym_names_index, sym_num);
-    for i in 0..sym_num {
-        let sym_name_cstr = CStr::from_ptr(sym_names.add(sym_names_index_list[i] as usize) as _);
-        let sym_name = sym_name_cstr.to_str().unwrap();
-        if sym_name == symbol {
-            return Some(kallsyms_address_list[i]);
+/// Get the address of the symbol.
+/// If not found, return None.
+pub fn addr_from_symbol(symbol: &str) -> Option<usize> {
+    let ksym = KSYM.get().unwrap();
+    for (name, addr) in ksym {
+        if name == symbol {
+            return Some(*addr);
         }
     }
     None
